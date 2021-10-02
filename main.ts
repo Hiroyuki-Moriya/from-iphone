0c7278164fce66036634fbb10d5c9fdf86d44087
Moriya Hiroyuki <Hiroyuki.Moriya@Gmail.com>
2021-10-02 10:20:09 +0900

    ボタンA追加

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+})
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Heart)
     soundExpression.giggle.playUntilDone()
